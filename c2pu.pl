5b7d715de3481bc57ac80fa71d01e55ab1d7d3a8
nanakira <nanakira@users.noreply.github.com>
2015-10-21 01:17:52 +0900

    Implement StateTable without stack

--- c2pu.pl
+++ c2pu.pl
@@ -8,7 +8,7 @@ $outputfile =~ s/(.*)\.(.+)$/$1.pu/;
 open( OUT, ">", $outputfile )
   or die "Cannot open $outputfile with write permission: $!";
 
-print (OUT "\@startuml hsm.png\n\n");
+print "\@startuml hsm.png\n\n";
 
 while(<IN>) {
   if (/START_BASIC_HSM_STATE_TABLE/){
@@ -16,22 +16,37 @@ while(<IN>) {
   }
 }
 
-print (OUT "\n\@enduml\n");
+print "\n\@enduml\n";
 
 close(OUT);
 close(IN);
 
 
 sub parseStateTable {
+  my $cur_state = '';
+  my $cur_parent = '';
+  my $domain, my $parent, my $state;
   while(<IN>) {
     if (/END_BASIC_HSM_STATE_TABLE/) {
       return;
     }
-    elsif (/\((.*),\s*(.*),\s*(.*)\)/) {
-      print (OUT "$1\n");
-      print (OUT "$2\n");
-      print (OUT "$3\n");
-      print (OUT "$4\n\n");
+    elsif (/\(\s*(\S+)\s*,\s*(\S+)\s*,\s*(\S+)\s*\)/) {
+      $parent = $2; $state = $3;
+#      print "parent: $2, state: $3, cur_parent: $cur_parent, cur_state: $cur_state\n";
+      if ($cur_state eq '' && $cur_parent eq ''){
+        print "STATE $state \{";
+        $cur_state = $state;
+        $cur_parent = $parent;
+      }
+      elsif ($parent eq $cur_parent){
+        print "\nSTATE $state";
+        $cur_state = $state;
+      }
+      elsif ($parent eq $cur_state){
+        print "\{\nSTATE $state"};
+        $cur_parent = $parent;
+        $cur_state = $state;
+#      print ("$1, $2, $3\n");
     }
 #    print (OUT $_)
   }
